52d6e0908b2a53d3d912b1dd3d5a4a74d5009987
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2024-04-07 07:47:39 +0300

    create access token

--- src/user_blueprint.py
+++ src/user_blueprint.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, Response
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 from src.user_model import User
 import json
 from mongoengine.errors import NotUniqueError
@@ -38,7 +39,15 @@ def login():
         user = User.objects(email=data["email"]).first()
         if user:
             if check_password_hash(user.password, data["password"]):
-                return Response(json.dumps({"msg": "Login successful"}), status=200)
+                fullname = f"{user.givenName}  {user.surName}"
+                identity = {"fullname": fullname, "email": user.email}
+                access_token = create_access_token(identity=identity)
+                return Response(
+                    json.dumps(
+                        {"msg": "Login successful", "access_token": access_token}
+                    ),
+                    status=200,
+                )
         return Response(json.dumps({"msg": "Invalid credentials"}), status=400)
     except Exception as e:
         print(e)
